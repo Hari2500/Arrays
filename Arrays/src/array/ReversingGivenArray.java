626d7d33f61c35a690548d39d6b6fd9d80722537
Hari2500 <harikrishnan252000@gmail.com>
2025-08-09 13:55:31 +0530

    Changes

--- Arrays/src/array/ReversingGivenArray.java
+++ Arrays/src/array/ReversingGivenArray.java
@@ -14,11 +14,20 @@ public class ReversingGivenArray {
 			r--;
 		}
 	}
+	
+	static void revOrder(int [] n) {
+		for(int i = n.length-1; i>=0; i--) {
+			System.out.print(n[i] + " ");
+		}
+	}
 
 	public static void main(String[] args) {
 		int [] a = {1,2,3,4,5};
-		System.out.println(Arrays.toString(a));
+		System.out.println("Given array: " + Arrays.toString(a));
+		System.out.print("Reverse printed array: ");
+		revOrder(a);
+		System.out.println();
 		rev(a);
-		System.out.println(Arrays.toString(a));	
+		System.out.println("Reversed Array: " + Arrays.toString(a));
 	}
 }
