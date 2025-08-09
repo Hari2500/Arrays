de11464b2d52167fcccace71fa75dbcf1a17e349
Hari2500 <harikrishnan252000@gmail.com>
2025-08-09 20:50:37 +0530

    Adding prime numbers in the given array

---
+++ Arrays/src/array/SumOfPrimeNumbersInArray.java
@@ -0,0 +1,31 @@
+package array;
+
+public class SumOfPrimeNumbersInArray {
+
+	static boolean isPrime(int num) {
+
+		if (num < 2)
+			return false;
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+
+	}
+
+	public static void main(String[] args) {
+		// Sum of prime numbers
+		int[] given = { 4, 5, 2, 6, 8, 7, 11 };
+
+		int sum = 0;
+		for (int i = 0; i < given.length; i++) {
+			if (isPrime(given[i])) {
+				sum += given[i];
+			}
+		}
+		System.out.println("Sum of prime numbers is: " + sum);
+	}
+
+}
