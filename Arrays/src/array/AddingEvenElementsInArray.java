31f56a8feab5caab24e2f04cb08ae45aff1d7a34
Hari2500 <harikrishnan252000@gmail.com>
2025-08-09 14:48:19 +0530

    changed lignments

--- Arrays/src/array/AddingEvenElementsInArray.java
+++ Arrays/src/array/AddingEvenElementsInArray.java
@@ -1,21 +1,21 @@
 package array;
 
 public class AddingEvenElementsInArray {
-	
-	static void addingEven(int [] b) {
-		
+
+	static void addingEven(int[] b) {
+
 		int add = 0;
-		for(int i = 0; i<b.length;i++) {
-			if(b[i]%2 ==0)
-				add+=b[i];
+		for (int i = 0; i < b.length; i++) {
+			if (b[i] % 2 == 0)
+				add += b[i];
 		}
-		System.out.println("Sum of even elements in the array is: "+add);
+		System.out.println("Sum of even elements in the array is: " + add);
 	}
 
 	public static void main(String[] args) {
 		// Adding Even Elements
-		
-		int [] a = {1,2,3,4,5};
+
+		int[] a = { 1, 2, 3, 4, 5 };
 		addingEven(a);
 
 	}
