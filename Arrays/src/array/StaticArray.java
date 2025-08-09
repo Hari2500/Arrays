bac8b2d21cc7dd685b3b5f21d62e563a4ed65a8d
Hari2500 <harikrishnan252000@gmail.com>
2025-08-09 13:31:43 +0530

    Static way of creating array

---
+++ Arrays/src/array/StaticArray.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.Arrays;
+
+public class StaticArray {
+
+	public static void main(String[] args) {
+		// Static way of creating arrays
+		//Declaration and initialization of array cannot be done in different lines
+		int [] a = {1,2,3,4,5};
+		
+		System.out.println(Arrays.toString(a));
+		
+		String [] s = {"Harikrishnan","Chennai","SDE","Data Analyst"};
+		System.out.println(Arrays.toString(s));
+		
+	}
+
+}
